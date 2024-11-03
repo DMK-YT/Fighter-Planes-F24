f7357b36352fd25d8f34e43cb6beb744dd4d0c25
BranBuddy <brandonwahl111@gmail.com>
2024-11-03 16:16:49 -0500

    edited

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,15 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemyTwo;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        Instantiate(player, transform.position, Quaternion.identity);
+        InvokeRepeating("CreateEnemyTwo", 4f, 8f);
     }
 
     // Update is called once per frame
@@ -24,5 +27,9 @@ public class GameManager : MonoBehaviour
     void CreateEnemy()
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0), Quaternion.identity);
+    }
+    void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwo, new Vector3(Random.Range(-5f, 5f), 8f, 0), Quaternion.identity);
     }
 }
